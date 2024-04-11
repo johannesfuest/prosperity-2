ce228076c4798a4ea19d895b22eb51025a16522b
Nicolas Neudeck <nicolas.neudeck@outlook.de>
2024-04-11 12:06:01 +0200

    added airtrader

--- airtraider.py
+++ airtraider.py
@@ -14,7 +14,7 @@ class Trader:
         self.limits = {
             "AMETHYSTS": 20,
             "STARFRUIT": 20,
-        } 
+        }
         
     def print_input_state(self, state: TradingState):
         print("\nINPUT TRADING STATE")
@@ -45,6 +45,8 @@ class Trader:
         print("=================\n")
         result = {}
         traderData = {}
+        buy_thresholds = {}
+        sell_thresholds = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
@@ -54,6 +56,8 @@ class Trader:
             print("## Acceptable price : " + str(acceptable_price))
             print("## We Buy below : " + str(acceptable_price * (1 - buy_spread)) + " and sell above : " + str(acceptable_price * (1 + sell_spread)))
             print("## Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            buy_thresholds[product] = int(np.floor(acceptable_price * (1 - buy_spread)))
+            sell_thresholds[product] = int(np.ceil(acceptable_price * (1 + sell_spread)))
             if len(order_depth.sell_orders) != 0:
                 for ask, ask_amount in order_depth.sell_orders.items():
                     if int(ask) < acceptable_price * (1 - (buy_spread)):
@@ -66,28 +70,21 @@ class Trader:
                     if int(bid) > acceptable_price * (1 + (sell_spread)):
                         print("## SELL", str(bid_amount) + "x", bid)
                         orders.append(Order(product, math.ceil(bid), -bid_amount))
-
-            # lowball price is a price very low to which we would love to buy
-            lowball_price = math.ceil(acceptable_price * (1 -  5 * buy_spread))
-            # highball price is a price very high to which we would love to sell
-            highball_price = math.floor(acceptable_price * (1 + 5 * sell_spread))
-            print("## Lowball price : " + str(lowball_price) + ", Highball price : " + str(highball_price))
-
             
             result[product] = orders
             traderData[product] = generate_trader_data(state, product)
             
-        result = self.adjust_for_position_breaches_and_add_random_oders(result, lowball_price, highball_price, state, True)
+        result = self.adjust_for_position_breaches(result, state, True)
+        result = self.adjust_to_exploit_limits(
+            result, state, buy_thresholds, sell_thresholds)
         traderData = json.dumps(traderData)
         conversions = 1
         return result, conversions, traderData
     
-    def adjust_for_position_breaches_and_add_random_oders(self, results, lowball_price, highball_price, state, fill_until_position_breach=False):
+    def adjust_for_position_breaches(self, results, state, fill_until_position_breach=False):
         valid_orders = {}
         for product, orders in results.items():
             valid_orders[product] = []
-            buy_position_breach = False
-            sell_position_breach = False
             buy_orders = [order for order in orders if order.quantity > 0]
             buy_orders = sorted(buy_orders, key=lambda x: x.price)
             sell_orders = [order for order in orders if order.quantity < 0]
@@ -96,21 +93,16 @@ class Trader:
             tmp_cur_position = cur_position
             for buy_order in buy_orders:
                 if tmp_cur_position + buy_order.quantity > self.limits[product]:
-                    buy_position_breach = True
                     if fill_until_position_breach:
                         buy_order = Order(product, buy_order.price, self.limits[product] - tmp_cur_position)
                         valid_orders[product].append(buy_order)
-                        tmp_cur_position += buy_order.quantity
                         break
                 else:
                     valid_orders[product].append(buy_order)
                     tmp_cur_position += buy_order.quantity
-            if not buy_position_breach:
-                valid_orders[product].append(Order(product, lowball_price, self.limits[product] - tmp_cur_position))
             tmp_cur_position = cur_position
             for sell_order in sell_orders:
                 if tmp_cur_position + sell_order.quantity < (-self.limits[product]):
-                    sell_position_breach = True
                     if fill_until_position_breach:
                         sell_order = Order(product, sell_order.price, (-self.limits[product]) - tmp_cur_position)
                         valid_orders[product].append(sell_order)
@@ -119,9 +111,60 @@ class Trader:
                 else:
                     valid_orders[product].append(sell_order)
                     tmp_cur_position += sell_order.quantity
-            if not sell_position_breach:
-                valid_orders[product].append(Order(product, highball_price, (-self.limits[product]) - tmp_cur_position))
         return valid_orders
+    
+    def adjust_to_exploit_limits(self, results, state, buy_threshold, sell_threshold):
+        for product, orders, in results.items():
+            cur_position = state.position.get(product, 0)
+            # initally, all orders we have sent are based on the existing book and are thus guaranteed to execute
+            max_pos = cur_position
+            min_pos = cur_position
+            for order_index, order in enumerate(orders):
+                if order.quantity > 0:
+                    max_pos += order.quantity
+                else:
+                    min_pos += order.quantity
+            # If the max/min position after all orders execute is not at the limit on yet, we need to add more orders that
+            # we fill the remaining quantity with optimistic trades
+            n_sell_order = -min_pos - self.limits[product]
+            n_buy_order = self.limits[product] - max_pos
+            
+            # Next we find the best remaining bid/ask after our initial orders go through and undercut if possible or 
+            # add to it if not
+            if n_sell_order < 0:
+                found_best = False
+                for ask, ask_amount in state.order_depths[product].sell_orders.items():
+                    if ask <= buy_threshold[product]:
+                        continue
+                    elif ask <= sell_threshold[product]:
+                        found_best = True
+                        sell_order = Order(product, sell_threshold[product], n_sell_order)
+                        break
+                    else:
+                        found_best = True
+                        sell_order = Order(product, ask - 1, n_sell_order)
+                        break
+                if not found_best:
+                    sell_order = Order(product, sell_threshold[product] + 2, n_sell_order)
+                results[product].append(sell_order)
+            if n_buy_order > 0:
+                found_best = False
+                for bid, bid_amount in state.order_depths[product].buy_orders.items():
+                    if bid >= sell_threshold[product]:
+                        continue
+                    elif bid >= buy_threshold[product]:
+                        found_best = True
+                        buy_order = Order(product, buy_threshold[product], n_buy_order)
+                        break
+                    else:
+                        found_best = True
+                        buy_order = Order(product, bid + 1, n_buy_order)
+                        break
+                if not found_best:
+                    buy_order = Order(product, buy_threshold[product] - 2, n_buy_order)
+                results[product].append(buy_order)
+        return results
+            
     
 def get_product_edge(state, product, buy_sell):
     price_history = get_price_history_from_state(state, product)
